41f35d6f99372445c26068a012d16e41d100cd8b
chapaiprakriti <prakriti9745906712@gmail.com>
2025-07-20 21:35:46 +0545

    initial commit

--- final electromart/electro-mart/backend/db.js
+++ final electromart/electro-mart/backend/db.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 
 const pool = new Pool({
   host: process.env.DB_HOST,
+  //host
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
