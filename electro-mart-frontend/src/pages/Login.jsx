ed45851030f55a80b9e41b01ac13b31b79ca5df2
chapaiprakriti <prakriti9745906712@gmail.com>
2025-07-24 12:55:28 +0545

    Updated frontend files

--- electro-mart-frontend/src/pages/Login.jsx
+++ electro-mart-frontend/src/pages/Login.jsx
@@ -101,6 +101,7 @@ export default function Login() {
             onChange={(e) => setPassword(e.target.value)}
             required
           />
+          //on
 
           <button
           //button
