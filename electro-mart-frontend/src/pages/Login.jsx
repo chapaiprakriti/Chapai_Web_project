22740a3514646f23e812fdcc262c64c4ee465bb1
chapaiprakriti <prakriti9745906712@gmail.com>
2025-07-24 12:53:55 +0545

    Updated frontend files

--- electro-mart-frontend/src/pages/Login.jsx
+++ electro-mart-frontend/src/pages/Login.jsx
@@ -67,6 +67,7 @@ export default function Login() {
             <div className="mb-6 p-3 bg-red-600 bg-opacity-30 border border-red-400 text-red-100 rounded">
               {error}
             </div>
+            //div
           )}
           {success && (
             <div className="mb-6 p-3 bg-green-600 bg-opacity-30 border border-green-400 text-green-100 rounded">
