f2f990a8b39e85d12bcca7e9ffd99132e51284f0
chapaiprakriti <prakriti9745906712@gmail.com>
2025-07-25 20:47:59 +0545

    Updated frontend files

--- electro-mart-frontend/src/pages/ProductPage.jsx
+++ electro-mart-frontend/src/pages/ProductPage.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import ProductCard from '../components/ProductCard';
-
+//1
 export default function ProductPage() {
   const [products, setProducts] = useState([]);
 
